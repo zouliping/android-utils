cd19f0cd301bbbe38de01d96f3711213afc3d3b0
zouliping <zoulp.ellie@gmail.com>
2016-12-14 10:55:00 +0800

    新建项目

---
+++ app/src/main/java/me/ellie/utils/MainActivity.java
@@ -0,0 +1,21 @@
+package me.ellie.utils;
+
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import me.ellie.utils.library.ClipboardUtil;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view) {
+    }
+}
